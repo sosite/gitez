39154fb24a2fba7b83cf2cb2fb4cbd5065ce21ec
sos <s_o_s_i_k@wp.pl>
2018-09-11 13:37:26 +0200

    Add some View extensions

---
+++ util/src/main/kotlin/com/socros/android/lib/util/ViewExt.kt
@@ -0,0 +1,70 @@
+package com.socros.android.lib.util
+
+import android.view.View
+import android.view.ViewStub
+import android.view.ViewTreeObserver
+import androidx.annotation.IdRes
+
+/* MAIN UTILS */
+
+enum class Visibility(val intValue: Int) {
+	GONE(View.GONE),
+	INVISIBLE(View.INVISIBLE),
+	VISIBLE(View.VISIBLE);
+
+	companion object : EnumValueCompanion<Int, Visibility>(
+			Visibility.values().associateBy(Visibility::intValue), GONE)
+}
+
+inline var View.visible: Boolean
+	get() = visibility == View.VISIBLE
+	set(value) {
+		visibility = if (value) View.VISIBLE else View.GONE
+	}
+
+inline var View.visibilityEnum: Visibility
+	get() = Visibility.fromValue(visibility)
+	set(value) {
+		visibility = value.intValue
+	}
+
+val View.hasTag: Boolean
+	get() = tag != null
+
+inline fun <T : View> T.afterMeasured(crossinline action: T.() -> Unit) {
+	if (measuredWidth > 0 || measuredHeight > 0) {
+		action()
+
+	} else {
+		viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+			override fun onGlobalLayout() {
+				if (measuredWidth > 0 || measuredHeight > 0) {
+					viewTreeObserver.removeOnGlobalLayoutListener(this)
+					action()
+				}
+			}
+		})
+	}
+}
+
+val ViewStub.inflated: Boolean
+	get() = parent == null
+
+fun View.traverseUpToFindParent(@IdRes id: Int): View? {
+	return traverseUpToFindParent { this.id == id }
+}
+
+fun View.traverseUpToFindParentOrThrow(@IdRes id: Int): View {
+	return traverseUpToFindParent { this.id == id }
+			?: throw IllegalArgumentException("Can't find parent with specified id!")
+}
+
+tailrec fun View?.traverseUpToFindParent(predicate: View.() -> Boolean): View? {
+	return if (this == null || predicate(this)) this
+	else (parent as? View).traverseUpToFindParent(predicate)
+}
+
+fun View?.traverseUpToFindParentOrThrow(predicate: View.() -> Boolean): View {
+	return this?.traverseUpToFindParent(predicate)
+			?: throw IllegalArgumentException("Can't find parent with specified predicate!")
+}
