0e294b0a742f2c84cfef1582217fc853af22abb4
sos <s_o_s_i_k@wp.pl>
2018-09-20 22:59:53 +0200

    Show search icon

--- contentsearch/src/main/kotlin/com/socros/android/app/gitez/contentsearch/view/ContentListActivity.kt
+++ contentsearch/src/main/kotlin/com/socros/android/app/gitez/contentsearch/view/ContentListActivity.kt
@@ -1,7 +1,7 @@
 package com.socros.android.app.gitez.contentsearch.view
 
-import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.Menu
 import com.socros.android.app.gitez.base.view.BaseActivity
 import com.socros.android.app.gitez.contentsearch.R
 import kotlinx.android.synthetic.main.content_list_activity.toolbar
@@ -10,10 +10,14 @@ class ContentListActivity : BaseActivity() {
 
 	override val layoutResId = R.layout.content_list_activity
 
-	@SuppressLint("InlinedApi")
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setSupportActionBar(toolbar)
+	}
+
+	override fun onCreateOptionsMenu(menu: Menu): Boolean {
+		menuInflater.inflate(R.menu.contentsearch, menu)
+		return super.onCreateOptionsMenu(menu)
 	}
 
 }
