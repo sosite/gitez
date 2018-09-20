9ce1ecec6956809c56a7dea266788918cfb3adf1
sos <s_o_s_i_k@wp.pl>
2018-09-21 01:09:18 +0200

    Open search action view via standalone button

--- contentsearch/src/main/kotlin/com/socros/android/app/gitez/contentsearch/view/ContentListActivity.kt
+++ contentsearch/src/main/kotlin/com/socros/android/app/gitez/contentsearch/view/ContentListActivity.kt
@@ -2,22 +2,32 @@ package com.socros.android.app.gitez.contentsearch.view
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import com.socros.android.app.gitez.base.view.BaseActivity
 import com.socros.android.app.gitez.contentsearch.R
+import kotlinx.android.synthetic.main.content_list_activity.searchBtn
 import kotlinx.android.synthetic.main.content_list_activity.toolbar
 
 class ContentListActivity : BaseActivity() {
 
 	override val layoutResId = R.layout.content_list_activity
 
+	private lateinit var searchMenu: MenuItem
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setSupportActionBar(toolbar)
+		initView()
 	}
 
 	override fun onCreateOptionsMenu(menu: Menu): Boolean {
 		menuInflater.inflate(R.menu.contentsearch, menu)
+		searchMenu = menu.findItem(R.id.actionSearch)
 		return super.onCreateOptionsMenu(menu)
+	}
+
+	private fun initView() {
+		searchBtn.setOnClickListener { searchMenu.expandActionView() }
 	}
 
 }
