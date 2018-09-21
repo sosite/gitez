dbe4449a30551b186fa34a8b8187e574c8ad1be4
sos <s_o_s_i_k@wp.pl>
2018-09-21 17:05:26 +0200

    Remove unnecessary function

--- base/src/main/kotlin/com/socros/android/app/gitez/base/view/BaseActivity.kt
+++ base/src/main/kotlin/com/socros/android/app/gitez/base/view/BaseActivity.kt
@@ -3,11 +3,9 @@ package com.socros.android.app.gitez.base.view
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.Fragment
 import com.socros.android.lib.androidcore.view.ACActivity
 import com.socros.android.lib.util.ApiVersion.V26_OREO
 import com.socros.android.lib.util.currentApi
-import com.socros.android.lib.util.inTransaction
 
 /**
  * Activity that handle light bottom navigation bar programmatically on Android Oreo V26
@@ -20,9 +18,4 @@ abstract class BaseActivity : ACActivity() {
 		}
 		super.onCreate(savedInstanceState)
 	}
-
-	protected fun addFragment(savedInstanceState: Bundle?, fragmentContainerId: Int, fragmentBuilder: () -> Fragment) =
-			savedInstanceState
-					?: supportFragmentManager.inTransaction { add(fragmentContainerId, fragmentBuilder()) }
-
 }
