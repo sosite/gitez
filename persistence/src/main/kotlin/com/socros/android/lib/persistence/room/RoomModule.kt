215018fdaa6fa60a3645683fd8dae092408c0885
sos <s_o_s_i_k@wp.pl>
2018-09-27 12:16:28 +0200

    A more concise return value of buildDb() extension

--- persistence/src/main/kotlin/com/socros/android/lib/persistence/room/RoomModule.kt
+++ persistence/src/main/kotlin/com/socros/android/lib/persistence/room/RoomModule.kt
@@ -6,5 +6,5 @@ import androidx.room.RoomDatabase
 
 interface RoomModule
 
-inline fun <reified Db : RoomDatabase> RoomModule.buildDb(context: Context, dbName: String) =
+inline fun <reified Db : RoomDatabase> RoomModule.buildDb(context: Context, dbName: String): Db =
 		Room.databaseBuilder(context, Db::class.java, dbName).build()
