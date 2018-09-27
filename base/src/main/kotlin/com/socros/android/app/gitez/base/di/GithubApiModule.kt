58fc9db5a695ddedfa94ed2edf14219ab250c4c9
sos <s_o_s_i_k@wp.pl>
2018-09-27 12:16:52 +0200

    Create GithubApiModule

---
+++ base/src/main/kotlin/com/socros/android/app/gitez/base/di/GithubApiModule.kt
@@ -0,0 +1,15 @@
+package com.socros.android.app.gitez.base.di
+
+import com.socros.android.app.gitez.base.BuildConfig
+import com.socros.android.lib.network.api.ApiModule
+import com.socros.android.lib.network.api.buildRetrofit
+import dagger.Module
+import dagger.Provides
+
+@Module
+class GithubApiModule : ApiModule {
+
+	@Provides
+	internal fun provideRetrofit() = buildRetrofit(BuildConfig.URL_API, BuildConfig.DEBUG)
+
+}
