10391e6f30fdfb1340cbfad11198441b1f624de3
sos <s_o_s_i_k@wp.pl>
2018-09-27 16:16:25 +0200

    Add default error messages

--- base/src/main/kotlin/com/socros/android/app/gitez/base/view/DataStatus.kt
+++ base/src/main/kotlin/com/socros/android/app/gitez/base/view/DataStatus.kt
@@ -1,23 +1,39 @@
 package com.socros.android.app.gitez.base.view
 
+import com.socros.android.app.gitez.base.R
 import com.socros.android.lib.repository.Resource
 
-sealed class DataStatus {
-	object InProgress : DataStatus()
-	object Success : DataStatus()
+sealed class DataStatus(val hasData: Boolean) {
+	open class InProgress(hasData: Boolean) : DataStatus(hasData)
+	class Success(hasData: Boolean) : DataStatus(hasData)
 
-	sealed class Error : DataStatus() {
-		object NetworkError : Error()
-		object ServerError : Error()
-		abstract class FeatureError : Error()
+	sealed class Error(hasData: Boolean, val headerStringRes: Int, val detailsStringRes: Int, val buttonStringRes: Int)
+		: DataStatus(hasData) {
+
+		class NetworkError(
+				hasData: Boolean,
+				headerStringRes: Int = R.string.networkError_header,
+				detailsStringRes: Int = R.string.networkError_details,
+				buttonStringRes: Int = R.string.networkError_btn)
+			: Error(hasData, headerStringRes, detailsStringRes, buttonStringRes)
+
+		class ServerError(
+				hasData: Boolean,
+				headerStringRes: Int = R.string.serverError_header,
+				detailsStringRes: Int = R.string.serverError_details,
+				buttonStringRes: Int = R.string.serverError_btn)
+			: Error(hasData, headerStringRes, detailsStringRes, buttonStringRes)
+
+		abstract class FeatureError(hasData: Boolean, headerStringRes: Int, detailsStringRes: Int, buttonStringRes: Int)
+			: Error(hasData, headerStringRes, detailsStringRes, buttonStringRes)
 	}
 
 	companion object {
-		fun fromRepositoryResource(resource: Resource<*>) = when (resource) {
-			is Resource.LoadingInProgress -> InProgress
-			is Resource.Success -> Success
-			is Resource.NetworkError -> Error.NetworkError
-			is Resource.ServerError -> Error.ServerError
+		fun fromRepositoryResource(resource: Resource<*>, hasData: Boolean) = when (resource) {
+			is Resource.LoadingInProgress -> InProgress(hasData)
+			is Resource.Success -> Success(hasData)
+			is Resource.NetworkError -> Error.NetworkError(hasData)
+			is Resource.ServerError -> Error.ServerError(hasData)
 		}
 	}
 }
