b668423e703ca7d5031c71049bbf4a3cd2ba52dd
sos <s_o_s_i_k@wp.pl>
2018-09-28 06:12:19 +0200

    Display user avatar using glide

--- contentsearch/src/main/kotlin/com/socros/android/app/gitez/contentsearch/view/adapter/UserViewHolder.kt
+++ contentsearch/src/main/kotlin/com/socros/android/app/gitez/contentsearch/view/adapter/UserViewHolder.kt
@@ -1,12 +1,14 @@
 package com.socros.android.app.gitez.contentsearch.view.adapter
 
 import android.view.ViewGroup
+import com.socros.android.app.gitez.base.glide.GlideApp
 import com.socros.android.app.gitez.contentsearch.R
 import com.socros.android.app.gitez.contentsearch.R.string
 import com.socros.android.app.gitez.contentsearch.data.SearchItem
 import com.socros.android.app.gitez.contentsearch.data.UserItem
 import com.socros.android.lib.androidcore.view.recycler.BaseViewHolder
 import io.reactivex.Observer
+import kotlinx.android.synthetic.main.content_list_user_item.view.avatarImg
 import kotlinx.android.synthetic.main.content_list_user_item.view.userIdTxt
 import kotlinx.android.synthetic.main.content_list_user_item.view.userNameTxt
 
@@ -15,6 +17,12 @@ class UserViewHolder(parent: ViewGroup, itemClickObserver: Observer<in UserItem>
 
 	override fun updateView(item: UserItem) {
 		with(itemView) {
+			GlideApp.with(context)
+					.load(item.avatarURl)
+					.placeholder(R.drawable.item_img_placeholder)
+					.circleCrop()
+					.into(avatarImg)
+
 			userNameTxt.text = item.login
 			userIdTxt.text = res.getString(string.searchItem_idValue, item.id)
 		}
